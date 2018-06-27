aa464c6ca0490216531ce26d773a823710925e6e
devisrikomati <15pa1a1231@vishnu.edu.in>
2018-06-27 14:21:48 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -7,7 +7,10 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-
+"""User information id, name ,email, 
+   picture are stored in database
+   Here id should be unique and name, 
+   email not null values"""
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -15,27 +18,19 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
-
+"""Information about shoppingmall"""
 class Shoppingmall(Base):
     __tablename__ = 'shoppingmall'
-
+""" Taken class method to insert the data of shoppingmall"""
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'name': self.name,
-            'id': self.id,
-        }
-
-
+"""Information about clothes"""
 class Cloth(Base):
     __tablename__ = 'cloth_details'
-
+""" Taken class method"""
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
@@ -48,16 +43,17 @@ class Cloth(Base):
 
     @property
     def serialize(self):
-        """Return object data in easily serializeable format"""
+        """Return object data in easily serializeable format
+           Here serialize the data based on user_id and shoppingmall_id"""
         return {
             'name': self.name,
-            'description': self.description,
             'id': self.id,
+            'description': self.description,
             'price': self.price,
             'type': self.type,
         }
 
-
+"""database named as dresses"""
 engine = create_engine('sqlite:///dresses.db')
 
 
